b363f3a80a816c18d2b951ada029ee14b85d6e19
kruegerm97 <kruegermaurice97@gmail.com>
2023-06-18 15:46:59 +0200

    add web api for testing and catch block in controller

--- src/main/java/maja/webtech/ApiController.java
+++ src/main/java/maja/webtech/ApiController.java
@@ -5,13 +5,17 @@ import maja.webtech.entities.Playlist;
 import maja.webtech.entities.Track;
 import maja.webtech.entities.User;
 import org.apache.tomcat.util.json.JSONParser;
+import org.apache.tomcat.util.json.ParseException;
 import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ApiController {
@@ -42,16 +46,19 @@ public class ApiController {
                 }
                 JSONParser parse = new JSONParser(line);
                 JSONObject jobj = (JSONObject)parse.parse();
-                String playlistId = (String) jobj.get("id");
+                String myPlaylistId = (String) jobj.get("id");
                 String playlistHref = (String) jobj.get("href");
                 String name = (String) jobj.get("name");
                 JSONObject trackObject = (JSONObject) jobj.get("tracks");
-                Track[] tracks;
-                trackObject.forEach(item -> {
-
-                });
-                Track[] tracks = (Track) trackObject.get();
+                List<Track> tracks = new ArrayList<>();
+//                trackObject.forEach(item -> {
+//                    tracks.add()
+//                });
+//                Track[] tracks = (Track) trackObject.get();
             }
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException(e);
         }
+        return null;
     }
 }
